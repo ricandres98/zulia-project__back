bb9fbde4111bf09a43d076e2f592bce5fe6314f5
Ricardo <ricardo270298@gmail.com>
2025-03-25 17:05:14 -0400

    -[MINOR FIX] Minor fixes in email service

--- src/routes/verification.router.ts
+++ src/routes/verification.router.ts
@@ -12,30 +12,38 @@ router.post(
 	"/verify-email",
 	validatorHandler(verifyEmailSchema, "body"),
 	async (req, res, next) => {
-	try {
-		const body = req.body;
-		const rta = await service.verifyEmail(body);
-		res.json(rta);
-	} catch (error) {
-		next(error);
-	}
-});
+		try {
+			const body = req.body;
+			const rta = await service.verifyEmail(body);
+			if (rta) {
+				res.status(200).json({
+					message: "Code sent"
+				});
+			} else {
+				res.status(400).json({
+					message: "An error ocurred"
+				});
+			}
+		} catch (error) {
+			next(error);
+		}
+	});
 
 router.post(
 	"/verify-code",
 	validatorHandler(verifyCodeSchema, "body"),
 	async (req, res, next) => {
-	try {
-		const body = req.body;
-		const rta = await service.verifyCode(body);
-		if (rta.status) {
-			res.status(200).json({message: rta.message});
-		} else {
-			res.status(400).json({message: rta.message})
+		try {
+			const body = req.body;
+			const rta = await service.verifyCode(body);
+			if (rta.status) {
+				res.status(200).json({ message: rta.message });
+			} else {
+				res.status(400).json({ message: rta.message })
+			}
+		} catch (error) {
+			next(error);
 		}
-	} catch (error) {
-		next(error);
-	}
-});
+	});
 
 export default router;
